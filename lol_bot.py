d961a8a7d245ce4c541e25b41ac0549394f1b0b8
Kenni Bawden <kenni.bawden@gmail.com>
2018-12-22 13:08:11 +1100

    Update lol_bot.py

--- lol_bot.py
+++ lol_bot.py
@@ -5,11 +5,11 @@ from flask import Flask, request
 app = Flask(__name__)
 
 # You can message lol_bot via <your website>/lol
-@app.route('/lol')
+@app.route('/lol', methods=['POST'])
 def lol_bot():
     # Get the value of the 'text' query parameter
     # request.args is a dictionary (cool!)
-    text = request.args.get('text')
+    text = request.form.get('text')
     # This bot lols at every command it gets sent!
     return f'lol {text}'
 
