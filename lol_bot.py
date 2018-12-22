41d58492ce9b5f9efe39fdcb15c736577c2137ba
Kenni Bawden <kenni.bawden@gmail.com>
2018-12-22 12:59:28 +1100

    Added MVP bot

---
+++ lol_bot.py
@@ -0,0 +1,19 @@
+# Import flask with the request object
+from flask import Flask, request
+
+# Create the web server
+app = Flask(__name__)
+
+# You can message lol_bot via <your website>/lol
+@app.route('/lol', methods=['POST'])
+def lol_bot():
+    # Get the value of the 'text' query parameter
+    # request.args is a dictionary (cool!)
+    text = request.form.get('text')
+    # This bot lols at every command it gets sent!
+    return f'lol {text}'
+
+
+
+# Start the web server!
+app.run()
